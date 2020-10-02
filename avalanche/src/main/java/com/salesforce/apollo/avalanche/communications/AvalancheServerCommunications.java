062dac85a68a240a072316b31a300e8c106c576d
Hellblazer <hellblazer@me.com>
2020-10-01 20:17:05 -0700

    Context scoped Avalanche

--- avalanche/src/main/java/com/salesforce/apollo/avalanche/communications/AvalancheServerCommunications.java
+++ avalanche/src/main/java/com/salesforce/apollo/avalanche/communications/AvalancheServerCommunications.java
@@ -8,6 +8,9 @@ package com.salesforce.apollo.avalanche.communications;
 
 import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import com.google.protobuf.ByteString;
@@ -17,9 +20,12 @@ import com.salesfoce.apollo.proto.DagNodes.Builder;
 import com.salesfoce.apollo.proto.Query;
 import com.salesfoce.apollo.proto.QueryResult;
 import com.salesforce.apollo.avalanche.Avalanche.Service;
+import com.salesforce.apollo.membership.Member;
 import com.salesforce.apollo.protocols.ClientIdentity;
 import com.salesforce.apollo.protocols.HashKey;
 
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 
 /**
@@ -27,39 +33,60 @@ import io.grpc.stub.StreamObserver;
  * @since 220
  */
 public class AvalancheServerCommunications extends AvalancheImplBase {
-    private final Service        avalanche;
     @SuppressWarnings("unused")
-    private final ClientIdentity identity;
+    private final ClientIdentity        identity;
+    private final Map<HashKey, Service> services = new ConcurrentHashMap<>();
+    private final Service               system;
 
     public AvalancheServerCommunications(Service avalanche, ClientIdentity identity) {
-        this.avalanche = avalanche;
+        this.system = avalanche;
         this.identity = identity;
     }
 
     @Override
     public void query(Query request, StreamObserver<QueryResult> responseObserver) {
-        QueryResult result = avalanche.onQuery(request.getTransactionsList()
-                                                      .stream()
-                                                      .map(e -> ByteBuffer.wrap(e.toByteArray()))
-                                                      .collect(Collectors.toList()),
-                                               request.getWantedList()
-                                                      .stream()
-                                                      .map(e -> new HashKey(e))
-                                                      .collect(Collectors.toList()));
-        responseObserver.onNext(result);
-        responseObserver.onCompleted();
+        evaluate(responseObserver, request.getContext(), s -> {
+            QueryResult result = s.onQuery(request.getTransactionsList()
+                                                  .stream()
+                                                  .map(e -> ByteBuffer.wrap(e.toByteArray()))
+                                                  .collect(Collectors.toList()),
+                                           request.getWantedList()
+                                                  .stream()
+                                                  .map(e -> new HashKey(e))
+                                                  .collect(Collectors.toList()));
+            responseObserver.onNext(result);
+            responseObserver.onCompleted();
+        });
+    }
+
+    public void register(Member member, Service service) {
+        services.computeIfAbsent(member.getId(), m -> service);
     }
 
     @Override
     public void requestDag(DagNodes request, StreamObserver<DagNodes> responseObserver) {
-        List<ByteBuffer> result = avalanche.requestDAG(request.getEntriesList()
-                                                              .stream()
-                                                              .map(e -> new HashKey(e))
-                                                              .collect(Collectors.toList()));
-        Builder builder = DagNodes.newBuilder();
-        result.forEach(e -> builder.addEntries(ByteString.copyFrom(e)));
-        responseObserver.onNext(builder.build());
-        responseObserver.onCompleted();
+        evaluate(responseObserver, request.getContext(), s -> {
+            List<ByteBuffer> result = s.requestDAG(request.getEntriesList()
+                                                          .stream()
+                                                          .map(e -> new HashKey(e))
+                                                          .collect(Collectors.toList()));
+            Builder builder = DagNodes.newBuilder();
+            result.forEach(e -> builder.addEntries(ByteString.copyFrom(e)));
+            responseObserver.onNext(builder.build());
+            responseObserver.onCompleted();
+        });
     }
 
+    private void evaluate(StreamObserver<?> responseObserver, ByteString context, Consumer<Service> c) {
+        Service service = getService(context);
+        if (service == null) {
+            responseObserver.onError(new StatusRuntimeException(Status.UNKNOWN));
+        } else {
+            c.accept(service);
+        }
+    }
+
+    private Service getService(ByteString context) {
+        return (context.isEmpty() && system != null) ? system : services.get(new HashKey(context));
+    }
 }
