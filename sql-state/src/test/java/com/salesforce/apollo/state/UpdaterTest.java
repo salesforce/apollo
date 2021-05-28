43b362cf56ecf705c1df0369cc72e519e7c41525
Hellblazer <hellblazer@me.com>
2021-05-28 10:15:41 -0700

    convert to Mutator

--- sql-state/src/test/java/com/salesforce/apollo/state/UpdaterTest.java
+++ sql-state/src/test/java/com/salesforce/apollo/state/UpdaterTest.java
@@ -43,11 +43,11 @@ public class UpdaterTest {
         statement.execute("create table books (id int, title varchar(50), author varchar(50), price float, qty int,  primary key (id))");
 
         Transaction.Builder builder = Transaction.newBuilder();
-        builder.setTxn(Any.pack(batch(batch("insert into books values (1001, 'Java for dummies', 'Tan Ah Teck', 11.11, 11)",
-                                            "insert into books values (1002, 'More Java for dummies', 'Tan Ah Teck', 22.22, 22)",
-                                            "insert into books values (1003, 'More Java for more dummies', 'Mohammad Ali', 33.33, 33)",
-                                            "insert into books values (1004, 'A Cup of Java', 'Kumar', 44.44, 44)",
-                                            "insert into books values (1005, 'A Teaspoon of Java', 'Kevin Jones', 55.55, 55)"))));
+        builder.setTxn(Any.pack(batch("insert into books values (1001, 'Java for dummies', 'Tan Ah Teck', 11.11, 11)",
+                                      "insert into books values (1002, 'More Java for dummies', 'Tan Ah Teck', 22.22, 22)",
+                                      "insert into books values (1003, 'More Java for more dummies', 'Mohammad Ali', 33.33, 33)",
+                                      "insert into books values (1004, 'A Cup of Java', 'Kumar', 44.44, 44)",
+                                      "insert into books values (1005, 'A Teaspoon of Java', 'Kevin Jones', 55.55, 55)")));
         Transaction transaction = builder.build();
 
         updater.getExecutor().execute(null, ExecutedTransaction.newBuilder().setTransaction(transaction).build(), null);
