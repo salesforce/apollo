847800e5a36c1fb33d094d09ae723296283479d7
Hellblazer <hellblazer@me.com>
2021-06-14 17:16:45 -0700

    remove modules frm build. Not the right model, anyway.  no longer useful
    Need to stop porting this until I'm ready for the web access exposure
    model.

--- apollo/src/main/java/com/salesforce/apollo/Apollo.java
+++ apollo/src/main/java/com/salesforce/apollo/Apollo.java
@@ -27,8 +27,7 @@ import com.salesforce.apollo.avalanche.Processor.TimedProcessor;
 import com.salesforce.apollo.comm.Router;
 import com.salesforce.apollo.fireflies.FireflyMetricsImpl;
 import com.salesforce.apollo.fireflies.Node;
-import com.salesforce.apollo.fireflies.View;
-import com.salesforce.apollo.protocols.HashKey;
+import com.salesforce.apollo.fireflies.View; 
 import com.salesforce.apollo.utils.Utils;
 
 /**
@@ -78,7 +77,7 @@ public class Apollo {
         Node node = identitySource.getNode();
         communications = c.communications.getComms(metrics, node, ForkJoinPool.commonPool());
         executor = ForkJoinPool.commonPool();
-        view = identitySource.createView(node, new HashKey(c.contextBase), communications,
+        view = identitySource.createView(node, c.contextBase, communications,
                                          new FireflyMetricsImpl(metrics), executor);
         seeds = identitySource.seeds();
         avalanche = new Avalanche(view, communications, c.avalanche, metrics == null ? null : new AvaMetrics(metrics),
