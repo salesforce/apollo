66ec63d0243477bd05d43c0407e01735c634e285
Hellblazer <hellblazer@me.com>
2021-06-17 08:57:03 -0700

    serialization of key coords to ByteString

--- stereotomy/src/main/java/com/salesforce/apollo/stereotomy/KeyCoordinates.java
+++ stereotomy/src/main/java/com/salesforce/apollo/stereotomy/KeyCoordinates.java
@@ -8,30 +8,18 @@ package com.salesforce.apollo.stereotomy;
 
 import static java.util.Objects.requireNonNull;
 
+import java.nio.ByteBuffer;
 import java.util.Objects;
 
+import com.google.protobuf.ByteString;
 import com.salesforce.apollo.stereotomy.event.EstablishmentEvent;
 import com.salesforce.apollo.stereotomy.event.EventCoordinates;
-import com.salesforce.apollo.stereotomy.identifier.BasicIdentifier;
 
 /**
  * @author hal.hildebrand
  *
  */
 public class KeyCoordinates {
-
-    public static KeyCoordinates convert(KeyCoordinates coordinates) {
-        if (coordinates instanceof KeyCoordinates) {
-            return (KeyCoordinates) coordinates;
-        }
-
-        return new KeyCoordinates(coordinates.getEstablishmentEvent(), coordinates.getKeyIndex());
-    }
-
-    public static KeyCoordinates of(BasicIdentifier basicIdentifier) {
-        EventCoordinates coordinates = EventCoordinates.of(basicIdentifier);
-        return new KeyCoordinates(coordinates, 0);
-    }
 
     public static KeyCoordinates of(EstablishmentEvent establishmentEvent, int keyIndex) {
         EventCoordinates coordinates = EventCoordinates.of(establishmentEvent);
@@ -40,6 +28,10 @@ public class KeyCoordinates {
 
     private final EventCoordinates establishmentEvent;
     private final int              keyIndex;
+
+    public KeyCoordinates(ByteBuffer buff) {
+        this(EventCoordinates.from(buff), buff.getInt());
+    }
 
     public KeyCoordinates(EventCoordinates establishmentEvent, int keyIndex) {
         if (keyIndex < 0) {
@@ -73,6 +65,13 @@ public class KeyCoordinates {
     @Override
     public int hashCode() {
         return Objects.hash(establishmentEvent, keyIndex);
+    }
+
+    public ByteString toByteString() {
+        ByteBuffer ki = ByteBuffer.allocate(2);
+        ki.putInt(keyIndex);
+        ki.flip();
+        return establishmentEvent.toByteString().concat(ByteString.copyFrom(ki));
     }
 
     @Override
