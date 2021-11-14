fff1586776aa7e88c1641328a6553b9718b8f336
Hellblazer <hellblazer@me.com>
2021-11-13 18:56:35 -0800

    Add ExponentialBackoff utility for clients

---
+++ utils/src/main/java/com/salesforce/apollo/utils/ExponentialBackoff.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+package com.salesforce.apollo.utils;
+
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * @author hal.hildebrand
+ *
+ */
+public class ExponentialBackoff<T> {
+
+    public static final class Builder<T> {
+        private long                base             = DEFAULT_WAIT_BASE_MILLIS;
+        private long                cap              = DEFAULT_WAIT_CAP_MILLIS;
+        private Consumer<Throwable> exceptionHandler = t -> {
+                                                     };
+        private boolean             infinite         = false;
+        private boolean             jitter           = false;
+        private int                 maxAttempts      = DEFAULT_MAX_ATTEMPTS;
+        private Predicate<T>        retryIf          = t -> false;
+
+        private Builder() {
+        }
+
+        public ExponentialBackoff<T> build() {
+            return new ExponentialBackoff<>(cap, base, maxAttempts, jitter, infinite, retryIf, exceptionHandler);
+        }
+
+        public void execute(Callable<T> task, CompletableFuture<T> futureSailor, ScheduledExecutorService scheduler) {
+            build().execute(task, futureSailor, scheduler);
+        }
+
+        public Builder<T> retryIf(final Predicate<T> retryIf) {
+            this.retryIf = Objects.requireNonNull(retryIf);
+            return this;
+        }
+
+        public Builder<T> setBase(final long base) {
+            this.base = base;
+            return this;
+        }
+
+        public Builder<T> setCap(final long cap) {
+            this.cap = cap;
+            return this;
+        }
+
+        public Builder<T> setExceptionHandler(final Consumer<Throwable> exceptionHandler) {
+            this.exceptionHandler = Objects.requireNonNull(exceptionHandler);
+            return this;
+        }
+
+        public Builder<T> setInfiniteAttempts() {
+            this.infinite = true;
+            return this;
+        }
+
+        public Builder<T> setJitter() {
+            this.jitter = true;
+            return this;
+        }
+
+        public Builder<T> setMaxAttempts(final int maxAttempts) {
+            this.maxAttempts = maxAttempts;
+            return this;
+        }
+    }
+
+    public static class RetryFailed extends Exception {
+        private static final long serialVersionUID = 1L;
+
+        public RetryFailed() {
+        }
+
+        public RetryFailed(String message) {
+            super(message);
+        }
+
+    }
+
+    static final int  DEFAULT_MAX_ATTEMPTS     = 10;
+    static final long DEFAULT_WAIT_BASE_MILLIS = 100;
+    static final long DEFAULT_WAIT_CAP_MILLIS  = 60000;
+
+    public static <T> Builder<T> newBuilder() {
+        return new Builder<>();
+    }
+
+    static long getWaitTime(final long cap, final long base, final long n) {
+        final long expWait = ((long) Math.pow(2, n)) * base;
+        return expWait <= 0 ? cap : Math.min(cap, expWait);
+    }
+
+    static long getWaitTimeWithJitter(final long cap, final long base, final long n) {
+        return ThreadLocalRandom.current().nextLong(0, getWaitTime(cap, base, n));
+    }
+
+    private final long                base;
+    private final long                cap;
+    private final Consumer<Throwable> exceptionHandler;
+    private final boolean             infinite;
+    private final boolean             jitter;
+    private final int                 maxAttempts;
+    private final Predicate<T>        retryIf;
+
+    public ExponentialBackoff(final long cap, final long base, final int maxAttempts, final boolean jitter,
+                              final boolean infinite, final Predicate<T> retryIf,
+                              Consumer<Throwable> exceptionHandler) {
+        this.cap = cap;
+        this.base = base;
+        this.maxAttempts = maxAttempts;
+        this.jitter = jitter;
+        this.infinite = infinite;
+        this.exceptionHandler = exceptionHandler;
+        this.retryIf = Objects.requireNonNull(retryIf);
+    }
+
+    public void execute(Callable<T> task, CompletableFuture<T> futureSailor, ScheduledExecutorService scheduler) {
+        if (infinite) {
+            execute(attempt -> true, 0, futureSailor, task, scheduler);
+        } else {
+            execute(attempt -> attempt < maxAttempts, 0, futureSailor, task, scheduler);
+        }
+    }
+
+    private void execute(final Predicate<Long> predicate, final long attempt, final CompletableFuture<T> futureSailor,
+                         Callable<T> task, final ScheduledExecutorService scheduler) {
+        if (!predicate.test(attempt)) {
+            futureSailor.completeExceptionally(new RetryFailed("Exceeded maximum attempts"));
+            return;
+        }
+
+        try {
+            final T result = task.call();
+            if (!retryIf.test(result)) {
+                futureSailor.complete(result);
+            }
+            return;
+        } catch (final Exception e) {
+            exceptionHandler.accept(e);
+        }
+        final var nextAttempt = attempt + 1;
+        final long waitTime = jitter ? getWaitTimeWithJitter(cap, base, nextAttempt)
+                                     : getWaitTime(cap, base, nextAttempt);
+        scheduler.schedule(() -> execute(predicate, nextAttempt, futureSailor, task, scheduler), waitTime,
+                           TimeUnit.MILLISECONDS);
+    }
+}
---
+++ utils/src/test/java/com/salesforce/apollo/utils/ExponentialBackoffTest.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+package com.salesforce.apollo.utils;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.jupiter.api.Test;
+
+import com.salesforce.apollo.utils.ExponentialBackoff.RetryFailed;
+
+/**
+ * @author hal.hildebrand
+ *
+ */
+public class ExponentialBackoffTest {
+
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+    @Test
+    public void overflowIsHandledCorrectly() {
+        final long cap = 60000L;
+        final long base = 100L;
+
+        final long wait1 = ExponentialBackoff.getWaitTime(cap, base, Long.MAX_VALUE);
+        assertThat(wait1, is(equalTo(cap)));
+
+        // MIN_VALUE will be capped
+        final long wait2 = ExponentialBackoff.getWaitTime(cap, base, Long.MIN_VALUE);
+        assertThat(wait2, is(equalTo(cap)));
+
+        final long wait3 = ExponentialBackoff.getWaitTime(cap, base, Long.MIN_VALUE + 1);
+        assertThat(wait3, is(equalTo(cap)));
+
+        // -1 will be capped; Should result in min(60000, 2^(MAX_VALUE -1) * 100) =
+        // 60000
+        final long wait4 = ExponentialBackoff.getWaitTime(cap, base, -1);
+        assertThat(wait4, is(equalTo(cap)));
+    }
+
+    @Test
+    public void successfulAfterThreeAttempts() throws Exception {
+        final AtomicInteger attempts = new AtomicInteger(0);
+        final AtomicInteger exceptions = new AtomicInteger(0);
+        CompletableFuture<String> futureSailor = new CompletableFuture<>();
+
+        ExponentialBackoff.<String>newBuilder().setBase(1).setCap(5000).setMaxAttempts(5)
+                          .setExceptionHandler(e -> exceptions.incrementAndGet()).execute(() -> {
+                              attempts.incrementAndGet();
+                              final boolean shouldThrow = attempts.get() < 3;
+                              if (shouldThrow) {
+                                  System.out.println("Throwing exception");
+                                  throw new RuntimeException("Fake exception");
+                              }
+                              return "Fake result";
+                          }, futureSailor, executor);
+
+        String result = futureSailor.get(1, TimeUnit.SECONDS);
+
+        assertThat(attempts.get(), is(equalTo(3)));
+        assertThat(exceptions.get(), is(equalTo(2)));
+        assertThat(result, is(equalTo("Fake result")));
+    }
+
+    @Test
+    public void testInfiniteAttempts() throws Exception {
+        final AtomicInteger attempts = new AtomicInteger(0);
+        final AtomicInteger exceptions = new AtomicInteger(0);
+        final Callable<String> task = () -> {
+            attempts.incrementAndGet();
+            // Just to ensure that we exceed the default max
+            // attempts
+            final boolean shouldThrow = attempts.get() < ExponentialBackoff.DEFAULT_MAX_ATTEMPTS + 1;
+            if (shouldThrow) {
+                System.out.println("Throwing exception");
+                throw new RuntimeException("Fake exception");
+            }
+            return "Fake result";
+        };
+        CompletableFuture<String> futureSailor = new CompletableFuture<>();
+
+        ExponentialBackoff.<String>newBuilder().setBase(1).setCap(10).setInfiniteAttempts()
+                          .setExceptionHandler(e -> exceptions.incrementAndGet()).execute(task, futureSailor, executor);
+
+        String result = futureSailor.get(1, TimeUnit.SECONDS);
+
+        assertThat(attempts.get(), is(equalTo(ExponentialBackoff.DEFAULT_MAX_ATTEMPTS + 1)));
+        assertThat(exceptions.get(), is(equalTo(ExponentialBackoff.DEFAULT_MAX_ATTEMPTS)));
+        assertThat(result, is(equalTo("Fake result")));
+    }
+
+    @Test
+    public void testSuccessfulExecution() throws Exception {
+        CompletableFuture<String> futureSailor = new CompletableFuture<>();
+        ExponentialBackoff.<String>newBuilder().setBase(100).setCap(5000).setMaxAttempts(5).setJitter()
+                          .setExceptionHandler(Throwable::printStackTrace)
+                          .execute(() -> "Do something", futureSailor, executor);
+
+        String result = futureSailor.get(1, TimeUnit.SECONDS);
+
+        assertThat(result, is(equalTo("Do something")));
+    }
+
+    @Test
+    public void testThatMaxAttemptsAreExceeded() throws Throwable {
+        CompletableFuture<Long> futureSailor = new CompletableFuture<>();
+        ExponentialBackoff.<Long>newBuilder().setMaxAttempts(3).setBase(1)
+                          .setExceptionHandler(e -> System.out.println(e.getMessage())).execute(() -> {
+                              throw new RuntimeException("Fake exception");
+                          }, futureSailor, executor);
+
+        try {
+            futureSailor.get(1, TimeUnit.SECONDS);
+            fail("should have completed exceptionally");
+        } catch (InterruptedException e) {
+            throw e;
+        } catch (ExecutionException e) {
+            if (!(e.getCause() instanceof RetryFailed)) {
+                throw e.getCause();
+            }
+        } catch (TimeoutException e) {
+            throw e;
+        }
+    }
+}
