702afd935a721155cc4a4446d0c305aef4be7f35
Hellblazer <hellblazer@me.com>
2021-12-19 12:23:46 -0800

    Optimized edges and insert logic

--- delphinius/src/main/java/com/salesforce/apollo/delphinius/Oracle.java
+++ delphinius/src/main/java/com/salesforce/apollo/delphinius/Oracle.java
@@ -38,124 +38,111 @@ public class Oracle {
         this.dslCtx = dslCtx;
     }
 
-    public void addTuple(String n, String p, String r, String c) throws SQLException {
+    public void addTuple(String n, String p, String c) throws SQLException {
         dslCtx.transaction(ctx -> {
             var context = DSL.using(ctx);
-            context.mergeInto(NAMESPACE).using(context.selectOne()).on(NAMESPACE.NAME.eq(n))
-                   .whenNotMatchedThenInsert(NAMESPACE.NAME).values(n).execute();
-            var namespace = context.select(NAMESPACE.ID).from(NAMESPACE).where(NAMESPACE.NAME.eq(n)).fetchOne()
-                                   .value1();
+            Table<org.jooq.Record> candidates = DSL.table(DSL.name("candidates"));
+            Field<Long> cParent = DSL.field(DSL.name("candidates", "parent"), Long.class);
+            Field<Long> cChild = DSL.field(DSL.name("candidates", "child"), Long.class);
+            try {
+                context.mergeInto(NAMESPACE).using(context.selectOne()).on(NAMESPACE.NAME.eq(n))
+                       .whenNotMatchedThenInsert(NAMESPACE.NAME).values(n).execute();
+//            var namespace = context.select(NAMESPACE.ID).from(NAMESPACE).where(NAMESPACE.NAME.eq(n)).fetchOne()
+//                                   .value1();
 
-            context.mergeInto(SUBJECT).using(context.selectOne()).on(SUBJECT.NAME.eq(p))
-                   .whenNotMatchedThenInsert(SUBJECT.NAME).values(p).execute();
+                context.mergeInto(SUBJECT).using(context.selectOne()).on(SUBJECT.NAME.eq(p))
+                       .whenNotMatchedThenInsert(SUBJECT.NAME).values(p).execute();
 
-            context.mergeInto(RELATION).using(context.selectOne()).on(RELATION.NAMESPACE.eq(namespace))
-                   .and(RELATION.NAME.eq(r)).whenNotMatchedThenInsert(RELATION.NAMESPACE, RELATION.NAME)
-                   .values(namespace, r).execute();
+                context.mergeInto(SUBJECT).using(context.selectOne()).on(SUBJECT.NAME.eq(c))
+                       .whenNotMatchedThenInsert(SUBJECT.NAME).values(c).execute();
 
-            context.mergeInto(SUBJECT).using(context.selectOne()).on(SUBJECT.NAME.eq(c))
-                   .whenNotMatchedThenInsert(SUBJECT.NAME).values(c).execute();
+                var parent = context.select(SUBJECT.ID).from(SUBJECT).where(SUBJECT.NAME.eq(p)).fetchOne().value1();
+                var child = context.select(SUBJECT.ID).from(SUBJECT).where(SUBJECT.NAME.eq(c)).fetchOne().value1();
 
-            var parent = context.select(SUBJECT.ID).from(SUBJECT).where(SUBJECT.NAME.eq(p)).fetchOne().value1();
-            var relation = context.select(RELATION.ID).from(RELATION).where(RELATION.NAMESPACE.eq(namespace))
-                                  .and(RELATION.NAME.eq(r)).fetchOne().value1();
-            var child = context.select(SUBJECT.ID).from(SUBJECT).where(SUBJECT.NAME.eq(c)).fetchOne().value1();
+                if (context.fetchExists(context.select(EDGE.ID).from(EDGE).where(EDGE.PARENT.eq(parent))
+                                               .and(EDGE.CHILD.eq(child)).and(EDGE.HOPS.eq(0)))) {
+                    return;
+                }
+                if (parent == child ||
+                    context.fetchExists(context.select(EDGE.ID).from(EDGE).where(EDGE.PARENT.eq(parent))
+                                               .and(EDGE.CHILD.eq(child)))) {
+                    throw new SQLException(String.format("Cycle inserting: %s to: %s", parent, child));
+                }
 
-            if (parent == child || context.fetchExists(context.select(EDGE.ID).from(EDGE).where(EDGE.PARENT.eq(parent))
-                                                              .and(EDGE.CHILD.eq(child)).and(EDGE.HOPS.eq(0)))) {
-                return;
+                context.createTemporaryTable(candidates).column(cParent).column(cChild).execute();
+
+                var A = EDGE.as("A");
+                var B = EDGE.as("B");
+                context.insertInto(candidates)
+                       .select(context.select(EDGE.PARENT, DSL.val(child)).from(EDGE).where(EDGE.CHILD.eq(parent))
+                                      .union(context.select(DSL.val(parent), EDGE.CHILD).from(EDGE)
+                                                    .where(EDGE.PARENT.endsWith(DSL.value(child))))
+                                      .union(context.select(A.PARENT, B.CHILD).from(A.crossJoin(B))
+                                                    .where(A.CHILD.eq(parent).and(B.PARENT.eq(child)))))
+                       .execute();
+
+                context.insertInto(EDGE).columns(EDGE.PARENT, EDGE.CHILD, EDGE.HOPS)
+                       .values(DSL.value(parent), DSL.value(child), DSL.value(0)).execute();
+
+                var E = EDGE.as("E");
+                context.insertInto(EDGE).columns(EDGE.PARENT, EDGE.CHILD, EDGE.HOPS)
+                       .select(context.select(cParent, cChild, DSL.val(1)).from(candidates)
+                                      .whereNotExists(context.select(E.HOPS).from(E).where(E.PARENT.eq(cParent))
+                                                             .and(E.CHILD.eq(cChild)).and(E.HOPS.eq(1))))
+                       .execute();
+            } finally {
+                context.dropTable(candidates).execute();
             }
-            if (context.fetchExists(context.select(EDGE.ID).from(EDGE).where(EDGE.PARENT.eq(parent))
-                                           .and(EDGE.CHILD.eq(child)))) {
-                throw new SQLException(String.format("Cycle inserting: %s rel: %s to: %s", parent, relation, child));
-            }
-            var id = context.insertInto(EDGE).columns(EDGE.PARENT, EDGE.CHILD, EDGE.HOPS).values(parent, child, 0)
-                            .returning().fetchOne().getId();
-
-            context.update(EDGE).set(EDGE.ENTRY, id).set(EDGE.EXIT, id).set(EDGE.DIRECT, id).where(EDGE.ID.eq(id))
-                   .execute();
-
-            dump(context, String.format("Initial insert: %s-%s-%s", p, r, c));
-
-            // step 1: parent's incoming edges to child
-            var edgeAsChild = EDGE.as("child");
-            context.insertInto(EDGE).columns(EDGE.ENTRY, EDGE.DIRECT, EDGE.EXIT, EDGE.PARENT, EDGE.CHILD, EDGE.HOPS)
-                   .select(context.select(edgeAsChild.ID, DSL.val(id), DSL.val(id), edgeAsChild.PARENT, DSL.val(child),
-                                          edgeAsChild.HOPS.plus(1))
-                                  .from(edgeAsChild).where(edgeAsChild.CHILD.eq(parent)))
-                   .execute();
-
-            dump(context, String.format("Parent incoming to child: %s-%s-%s", p, r, c));
-
-            // step 2: parent to child's outgoing edges
-            context.insertInto(EDGE).columns(EDGE.ENTRY, EDGE.DIRECT, EDGE.EXIT, EDGE.PARENT, EDGE.CHILD, EDGE.HOPS)
-                   .select(context.select(DSL.val(id), DSL.val(id), EDGE.EXIT, DSL.val(parent), EDGE.CHILD,
-                                          EDGE.HOPS.plus(1))
-                                  .from(EDGE).where(EDGE.PARENT.eq(child)))
-                   .execute();
-
-            dump(context, String.format("Parent to child's outgoing: %s-%s-%s", p, r, c));
-
-            // step 3: parent's incoming edges to end vertex of child's outgoing edges
-            var A = EDGE.as("parent");
-            var B = EDGE.as("child");
-            context.insertInto(EDGE).columns(EDGE.ENTRY, EDGE.DIRECT, EDGE.EXIT, EDGE.PARENT, EDGE.CHILD, EDGE.HOPS)
-                   .select(context.select(A.ENTRY, DSL.val(id), B.EXIT, A.PARENT, B.CHILD, A.HOPS.plus(B.HOPS).plus(2))
-                                  .from(A).crossJoin(B).where(A.CHILD.eq(parent)).and(B.PARENT.eq(child)))
-                   .execute();
-            dump(context, String.format("Parent's incoming to end vertex of child's outgoing: %s-%s-%s", p, r, c));
         });
     }
 
-    public void delete(String namespace, String p, String r, String c) {
-        dslCtx.transaction(ctx -> {
-            var context = DSL.using(ctx);
-            var resolved = context.select(OBJECT.ID, RELATION.ID, SUBJECT.ID).from(OBJECT, RELATION, SUBJECT)
-                                  .join(OBJECT).on(NAMESPACE.ID.eq(OBJECT.NAMESPACE)).where(OBJECT.NAME.eq(p))
-                                  .and(RELATION.NAME.eq(r)).and(SUBJECT.NAME.eq(c)).fetchOne();
-            if (resolved == null) {
-                throw new SQLException(String.format("Edge object: %s:%s rel: %s subject: %s does not exist", namespace,
-                                                     p, r, c));
-            }
-
-            var parent = resolved.value1();
-            var child = resolved.value3();
-
-            var idR = context.select(EDGE.ID).from(EDGE).where(EDGE.PARENT.eq(parent)).and(EDGE.CHILD.eq(child))
-                             .and(EDGE.HOPS.eq(0)).fetchOne();
-            if (idR == null) {
-                throw new SQLException(String.format("Edge object: %s:%s rel: %s subject: %s does not exist", namespace,
-                                                     p, r, c));
-            }
-            var id = idR.value1();
-
-            // Temporary table to track the purged
-            Table<org.jooq.Record> purgeList = DSL.table(DSL.name("purge_list"));
-            Field<Long> pID = DSL.field(DSL.name("purge_list", "id"), Long.class);
-            context.createTemporaryTable(purgeList);
-
-            // Rows that were originally inserted with the first add() call for this direct
-            // edge
-            context.insertInto(purgeList).select(context.select(EDGE.ID).from(EDGE).where(EDGE.DIRECT.eq(id)))
-                   .execute();
-
-            // Scan and find all dependent rows that are inserted afterwards
-            while (true) {
-                if (context.insertInto(purgeList)
-                           .select(context.select(EDGE.ID).from(EDGE)
-                                          .where(EDGE.HOPS.gt(0)
-                                                          .and(EDGE.ENTRY.in(context.select(pID).from(purgeList))
-                                                                         .or(EDGE.EXIT.in(context.select(pID)
-                                                                                                 .from(purgeList)))))
-                                          .and(EDGE.ID.notIn(context.select(pID).from(purgeList))))
-                           .execute() == 0) {
-                    break;
-                }
-            }
-
-            context.delete(EDGE).where(EDGE.ID.in(context.select(pID).from(purgeList))).execute();
-            context.dropTable(purgeList);
-        });
+    public void delete(String namespace, String p, String c) {
+//        dslCtx.transaction(ctx -> {
+//            var context = DSL.using(ctx);
+//            var resolved = context.select(OBJECT.ID, RELATION.ID, SUBJECT.ID).from(OBJECT, RELATION, SUBJECT)
+//                                  .join(OBJECT).on(NAMESPACE.ID.eq(OBJECT.NAMESPACE)).where(OBJECT.NAME.eq(p))
+//                                  .and(SUBJECT.NAME.eq(c)).fetchOne();
+//            if (resolved == null) {
+//                throw new SQLException(String.format("Edge parent: %s:%s child: %s does not exist", namespace, p, c));
+//            }
+//
+//            var parent = resolved.value1();
+//            var child = resolved.value3();
+//
+//            var idR = context.select(EDGE.ID).from(EDGE).where(EDGE.PARENT.eq(parent)).and(EDGE.CHILD.eq(child))
+//                             .and(EDGE.HOPS.eq(0)).fetchOne();
+//            if (idR == null) {
+//                throw new SQLException(String.format("Edge parent: %s:%s child: %s does not exist", namespace, p, c));
+//            }
+//            var id = idR.value1();
+//
+//            // Temporary table to track the purged
+//            Table<org.jooq.Record> purgeList = DSL.table(DSL.name("purge_list"));
+//            Field<Long> pID = DSL.field(DSL.name("purge_list", "id"), Long.class);
+//            context.createTemporaryTable(purgeList);
+//
+//            // Rows that were originally inserted with the first add() call for this direct
+//            // edge
+//            context.insertInto(purgeList).select(context.select(EDGE.ID).from(EDGE).where(EDGE.DIRECT.eq(id)))
+//                   .execute();
+//
+//            // Scan and find all dependent rows that are inserted afterwards
+//            while (true) {
+//                if (context.insertInto(purgeList)
+//                           .select(context.select(EDGE.ID).from(EDGE)
+//                                          .where(EDGE.HOPS.gt(0)
+//                                                          .and(EDGE.ENTRY.in(context.select(pID).from(purgeList))
+//                                                                         .or(EDGE.EXIT.in(context.select(pID)
+//                                                                                                 .from(purgeList)))))
+//                                          .and(EDGE.ID.notIn(context.select(pID).from(purgeList))))
+//                           .execute() == 0) {
+//                    break;
+//                }
+//            }
+//
+//            context.delete(EDGE).where(EDGE.ID.in(context.select(pID).from(purgeList))).execute();
+//            context.dropTable(purgeList);
+//        });
     }
 
     private void dump(DSLContext context, String string) {
--- delphinius/src/test/java/com/salesforce/apollo/delphinius/Questions3Test.java
+++ delphinius/src/test/java/com/salesforce/apollo/delphinius/Questions3Test.java
@@ -6,18 +6,17 @@
  */
 package com.salesforce.apollo.delphinius;
 
+import static com.salesforce.apollo.delphinius.schema.tables.Edge.EDGE;
+import static com.salesforce.apollo.delphinius.schema.tables.Subject.SUBJECT;
+
 import java.util.Properties;
+import java.util.Random;
 
 import org.h2.jdbc.JdbcConnection;
 import org.jooq.impl.DSL;
 import org.junit.Test;
 
 import com.salesforce.apollo.delphinius.schema.tables.Subject;
-
-import static com.salesforce.apollo.delphinius.schema.tables.Edge.*;
-import static com.salesforce.apollo.delphinius.schema.tables.Namespace.*;
-import static com.salesforce.apollo.delphinius.schema.tables.Relation.*;
-import static com.salesforce.apollo.delphinius.schema.tables.Subject.*;
 
 import liquibase.Liquibase;
 import liquibase.database.core.H2Database;
@@ -31,7 +30,8 @@ public class Questions3Test {
 
     @Test
     public void smokin() throws Exception {
-        final var url = "jdbc:h2:mem:test_engine-smoke;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1";
+        final var url = String.format("jdbc:h2:mem:test_engine-smoke-%s;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1",
+                                      new Random().nextLong());
         var connection = new JdbcConnection(url, new Properties(), "", "");
 
         var database = new H2Database();
@@ -42,22 +42,22 @@ public class Questions3Test {
         connection = new JdbcConnection(url, new Properties(), "", "");
 
         Oracle oracle = new Oracle(connection);
-        oracle.addTuple("foo", "HelpDesk", "member", "Admins");
-        oracle.addTuple("foo", "Ali", "member", "Admins");
-        oracle.addTuple("foo", "Ali", "member", "Users");
-        oracle.addTuple("foo", "Burcu", "member", "Users");
-        oracle.addTuple("foo", "Can", "member", "Users");
-        oracle.addTuple("foo", "Managers", "member", "Users");
-        oracle.addTuple("foo", "Technicians", "member", "Users");
-        oracle.addTuple("foo", "Demet", "member", "HelpDesk");
-        oracle.addTuple("foo", "Egin", "member", "HelpDesk");
-        oracle.addTuple("foo", "Egin", "member", "Users");
-        oracle.addTuple("foo", "Fuat", "member", "Managers");
-        oracle.addTuple("foo", "G l", "member", "Managers");
-        oracle.addTuple("foo", "Hakan", "member", "Technicians");
-        oracle.addTuple("foo", "Irmak", "member", "Technicians");
-        oracle.addTuple("foo", "ABCTechnicians", "member", "Technicians");
-        oracle.addTuple("foo", "Jale", "member", "ABCTechnicians");
+        oracle.addTuple("foo", "HelpDesk", "Admins");
+        oracle.addTuple("foo", "Ali", "Admins");
+        oracle.addTuple("foo", "Ali", "Users");
+        oracle.addTuple("foo", "Burcu", "Users");
+        oracle.addTuple("foo", "Can", "Users");
+        oracle.addTuple("foo", "Managers", "Users");
+        oracle.addTuple("foo", "Technicians", "Users");
+        oracle.addTuple("foo", "Demet", "HelpDesk");
+        oracle.addTuple("foo", "Egin", "HelpDesk");
+        oracle.addTuple("foo", "Egin", "Users");
+        oracle.addTuple("foo", "Fuat", "Managers");
+        oracle.addTuple("foo", "G l", "Managers");
+        oracle.addTuple("foo", "Hakan", "Technicians");
+        oracle.addTuple("foo", "Irmak", "Technicians");
+        oracle.addTuple("foo", "ABCTechnicians", "Technicians");
+        oracle.addTuple("foo", "Jale", "ABCTechnicians");
 
         var dsl = DSL.using(connection);
         Subject pa = SUBJECT.as("parent");
