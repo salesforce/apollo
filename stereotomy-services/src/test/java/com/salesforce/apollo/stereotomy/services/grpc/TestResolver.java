12981c7b24a5f07065d4eddd683e15b30252906e
Hellblazer <hellblazer@me.com>
2022-03-03 17:16:33 -0800

    test kerl provider api

--- stereotomy-services/src/test/java/com/salesforce/apollo/stereotomy/services/grpc/TestResolver.java
+++ stereotomy-services/src/test/java/com/salesforce/apollo/stereotomy/services/grpc/TestResolver.java
@@ -6,19 +6,39 @@
  */
 package com.salesforce.apollo.stereotomy.services.grpc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ForkJoinPool;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.salesforce.apollo.comm.LocalRouter;
 import com.salesforce.apollo.comm.ServerConnectionCache;
+import com.salesforce.apollo.crypto.Digest;
 import com.salesforce.apollo.crypto.DigestAlgorithm;
 import com.salesforce.apollo.membership.impl.SigningMemberImpl;
+import com.salesforce.apollo.stereotomy.EventCoordinates;
 import com.salesforce.apollo.stereotomy.KERL;
+import com.salesforce.apollo.stereotomy.KeyState;
+import com.salesforce.apollo.stereotomy.Stereotomy;
+import com.salesforce.apollo.stereotomy.StereotomyImpl;
+import com.salesforce.apollo.stereotomy.StereotomyKeyStore;
+import com.salesforce.apollo.stereotomy.event.KeyEvent;
+import com.salesforce.apollo.stereotomy.event.Seal.CoordinatesSeal;
+import com.salesforce.apollo.stereotomy.event.Seal.DigestSeal;
 import com.salesforce.apollo.stereotomy.identifier.SelfAddressingIdentifier;
+import com.salesforce.apollo.stereotomy.identifier.spec.InteractionSpecification;
+import com.salesforce.apollo.stereotomy.identifier.spec.RotationSpecification;
 import com.salesforce.apollo.stereotomy.mem.MemKERL;
+import com.salesforce.apollo.stereotomy.mem.MemKeyStore;
 import com.salesforce.apollo.stereotomy.services.impl.ProtoKERLService;
 import com.salesforce.apollo.utils.Utils;
 
@@ -27,9 +47,13 @@ import com.salesforce.apollo.utils.Utils;
  *
  */
 public class TestResolver {
+    KERL                     kel;
+    final StereotomyKeyStore ks = new MemKeyStore();
+    SecureRandom             secureRandom;
+
+    private LocalRouter clientRouter;
 
     private LocalRouter serverRouter;
-    private LocalRouter clientRouter;
 
     @AfterEach
     public void after() {
@@ -43,9 +67,60 @@ public class TestResolver {
         }
     }
 
+    @BeforeEach
+    public void before() throws Exception {
+        secureRandom = SecureRandom.getInstance("SHA1PRNG");
+        secureRandom.setSeed(new byte[] { 0 });
+        kel = new MemKERL(DigestAlgorithm.DEFAULT);
+    }
+
     @Test
-    public void smokin() throws Exception {
+    public void provider() throws Exception {
         var context = DigestAlgorithm.DEFAULT.getLast().prefix("foo");
+        var client = setup(context);
+        client.resolve(new SelfAddressingIdentifier(context));
+
+        Stereotomy controller = new StereotomyImpl(ks, kel, secureRandom);
+
+        var i = controller.newIdentifier().get();
+
+        var digest = DigestAlgorithm.BLAKE3_256.digest("digest seal".getBytes());
+        var event = EventCoordinates.of(kel.getKeyEvent(i.getLastEstablishmentEvent()).get());
+        var seals = List.of(DigestSeal.construct(digest), DigestSeal.construct(digest),
+                            CoordinatesSeal.construct(event));
+
+        i.rotate();
+        i.seal(InteractionSpecification.newBuilder());
+        i.rotate(RotationSpecification.newBuilder().addAllSeals(seals));
+        i.seal(InteractionSpecification.newBuilder().addAllSeals(seals));
+        i.rotate();
+        i.rotate();
+
+        var opti = client.kerl(i.getIdentifier());
+        assertNotNull(opti);
+        assertFalse(opti.isEmpty());
+        var iKerl = opti.get();
+        assertEquals(7, iKerl.size());
+        assertEquals(KeyEvent.INCEPTION_TYPE, iKerl.get(0).event().getIlk());
+        assertEquals(KeyEvent.ROTATION_TYPE, iKerl.get(1).event().getIlk());
+        assertEquals(KeyEvent.INTERACTION_TYPE, iKerl.get(2).event().getIlk());
+        assertEquals(KeyEvent.ROTATION_TYPE, iKerl.get(3).event().getIlk());
+        assertEquals(KeyEvent.INTERACTION_TYPE, iKerl.get(4).event().getIlk());
+        assertEquals(KeyEvent.ROTATION_TYPE, iKerl.get(5).event().getIlk());
+        assertEquals(KeyEvent.ROTATION_TYPE, iKerl.get(6).event().getIlk());
+
+        Optional<KeyState> keyState = client.resolve(i.getIdentifier());
+        assertNotNull(keyState);
+        assertFalse(keyState.isEmpty());
+        assertEquals(kel.getKeyState(i.getIdentifier()).get(), keyState.get());
+
+        keyState = client.resolve(i.getCoordinates());
+        assertNotNull(keyState);
+        assertFalse(keyState.isEmpty());
+        assertEquals(kel.getKeyState(i.getIdentifier()).get(), keyState.get());
+    }
+
+    private KERLClient setup(Digest context) {
         var prefix = UUID.randomUUID().toString();
 
         var serverMember = new SigningMemberImpl(Utils.getMember(0));
@@ -58,18 +133,14 @@ public class TestResolver {
         serverRouter.start();
         clientRouter.start();
 
-        KERL kerl = new MemKERL(DigestAlgorithm.DEFAULT);
-        ProtoKERLService protoService = new ProtoKERLService(kerl);
+        ProtoKERLService protoService = new ProtoKERLService(kel);
 
-        @SuppressWarnings("unused")
-        var serverComms = serverRouter.create(serverMember, context, protoService, r -> new KERLServer(null, r), null,
-                                              null);
+        serverRouter.create(serverMember, context, protoService, r -> new KERLServer(null, r), null, null);
 
         var clientComms = clientRouter.create(clientMember, context, protoService, r -> new KERLServer(null, r),
                                               KERLClient.getCreate(context, null), null);
 
         var client = clientComms.apply(serverMember, clientMember);
-
-        client.resolve(new SelfAddressingIdentifier(context));
+        return client;
     }
 }
