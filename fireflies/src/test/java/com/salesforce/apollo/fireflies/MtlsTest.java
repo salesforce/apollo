9dd2681d267b1a2d091c6e5e9b6e5cf406ac8ba6
Hellblazer <hellblazer@me.com>
2022-03-20 16:53:11 -0700

    MtlsTest now working

--- fireflies/src/test/java/com/salesforce/apollo/fireflies/MtlsTest.java
+++ fireflies/src/test/java/com/salesforce/apollo/fireflies/MtlsTest.java
@@ -9,9 +9,16 @@ package com.salesforce.apollo.fireflies;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.security.Provider;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -31,6 +38,7 @@ import org.junit.jupiter.api.Test;
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.collect.Sets;
+import com.salesfoce.apollo.fireflies.proto.Identity;
 import com.salesforce.apollo.comm.EndpointProvider;
 import com.salesforce.apollo.comm.MtlsRouter;
 import com.salesforce.apollo.comm.Router;
@@ -38,47 +46,72 @@ import com.salesforce.apollo.comm.ServerConnectionCache;
 import com.salesforce.apollo.comm.ServerConnectionCache.Builder;
 import com.salesforce.apollo.comm.ServerConnectionCacheMetricsImpl;
 import com.salesforce.apollo.comm.StandardEpProvider;
+import com.salesforce.apollo.comm.grpc.ClientContextSupplier;
+import com.salesforce.apollo.comm.grpc.MtlsServer;
+import com.salesforce.apollo.comm.grpc.ServerContextSupplier;
 import com.salesforce.apollo.crypto.Digest;
 import com.salesforce.apollo.crypto.DigestAlgorithm;
-import com.salesforce.apollo.crypto.Signer.SignerImpl;
+import com.salesforce.apollo.crypto.SignatureAlgorithm;
 import com.salesforce.apollo.crypto.cert.CertificateWithPrivateKey;
 import com.salesforce.apollo.crypto.ssl.CertificateValidator;
-import com.salesforce.apollo.fireflies.View.IdentityWrapper;
 import com.salesforce.apollo.fireflies.View.Participant;
 import com.salesforce.apollo.membership.Context;
 import com.salesforce.apollo.membership.Member;
-import com.salesforce.apollo.membership.SigningMember;
-import com.salesforce.apollo.membership.impl.SigningMemberImpl;
+import com.salesforce.apollo.membership.stereotomy.ControlledIdentifierMember;
+import com.salesforce.apollo.stereotomy.ControlledIdentifier;
+import com.salesforce.apollo.stereotomy.Stereotomy;
+import com.salesforce.apollo.stereotomy.StereotomyImpl;
+import com.salesforce.apollo.stereotomy.identifier.SelfAddressingIdentifier;
+import com.salesforce.apollo.stereotomy.mem.MemKERL;
+import com.salesforce.apollo.stereotomy.mem.MemKeyStore;
 import com.salesforce.apollo.stereotomy.services.EventValidation;
 import com.salesforce.apollo.utils.Utils;
 
 import io.grpc.netty.shaded.io.netty.handler.ssl.ClientAuth;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
 
 /**
  * @author hal.hildebrand
  * @since 220
  */
 public class MtlsTest {
-    private static final int                    CARDINALITY;
-    private static Map<Digest, IdentityWrapper> certs;
-    private static final boolean                LARGE_TESTS = Boolean.getBoolean("large_tests");
+    private static final int                                                   CARDINALITY;
+    private static Map<Digest, ControlledIdentifier<SelfAddressingIdentifier>> identities;
+    private static final Map<Digest, CertificateWithPrivateKey>                certs       = new HashMap<>();
+    private static final Map<Digest, InetSocketAddress>                        endpoints   = new HashMap<>();
+    private static final boolean                                               LARGE_TESTS = Boolean.getBoolean("large_tests");
+    private static final Random                                                entropy     = new Random(0x666);
     static {
-//        ProviderUtils.getProviderBC();
         CARDINALITY = LARGE_TESTS ? 100 : 10;
     }
 
     @BeforeAll
-    public static void beforeClass() {
-        certs = IntStream.range(0, CARDINALITY)
-                         .parallel()
-                         .mapToObj(i -> Utils.getMember(i))
-                         .collect(Collectors.toMap(cert -> Member.getMemberIdentifier(cert.getX509Certificate()),
-                                                   cert -> cert));
+    public static void beforeClass() throws Exception {
+        String localhost = InetAddress.getLocalHost().getHostName();
+        var stereotomy = new StereotomyImpl(new MemKeyStore(), new MemKERL(DigestAlgorithm.DEFAULT),
+                                            new SecureRandom());
+        identities = IntStream.range(0, CARDINALITY)
+                              .parallel()
+                              .mapToObj(i -> stereotomy.newIdentifier().get())
+                              .map(ci -> {
+                                  @SuppressWarnings("unchecked")
+                                  var casted = (ControlledIdentifier<SelfAddressingIdentifier>) ci;
+                                  return casted;
+                              })
+                              .collect(Collectors.toMap(controlled -> controlled.getIdentifier().getDigest(),
+                                                        controlled -> controlled));
+        identities.entrySet().forEach(e -> {
+            InetSocketAddress endpoint = new InetSocketAddress(localhost, Utils.allocatePort());
+            certs.put(e.getKey(),
+                      e.getValue()
+                       .provision(endpoint, Instant.now(), Duration.ofDays(1), SignatureAlgorithm.DEFAULT)
+                       .get());
+            endpoints.put(e.getKey(), endpoint);
+        });
     }
 
     private List<Router> communications = new ArrayList<>();
-
-    private List<View> views;
+    private List<View>   views;
 
     @AfterEach
     public void after() {
@@ -94,44 +127,66 @@ public class MtlsTest {
 
     @Test
     public void smoke() throws Exception {
-        Random entropy = new Random(0x666);
         MetricRegistry registry = new MetricRegistry();
         MetricRegistry node0Registry = new MetricRegistry();
 
-        List<IdentityWrapper> seeds = new ArrayList<>();
-        List<SigningMember> members = certs.values()
-                                           .stream()
-                                           .map(cert -> new SigningMemberImpl(Member.getMemberIdentifier(cert.getX509Certificate()),
-                                                                              cert.getX509Certificate(),
-                                                                              cert.getPrivateKey(),
-                                                                              new SignerImpl(cert.getPrivateKey()),
-                                                                              cert.getX509Certificate().getPublicKey()))
-                                           .collect(Collectors.toList());
-        assertEquals(certs.size(), members.size());
+        List<Identity> seeds = new ArrayList<>();
+        var members = identities.values().stream().map(identity -> new ControlledIdentifierMember(identity)).toList();
 
         var ctxBuilder = Context.<Participant>newBuilder().setCardinality(CARDINALITY);
 
         while (seeds.size() < ctxBuilder.build().getRingCount() + 1) {
-            IdentityWrapper identity = certs.get(members.get(entropy.nextInt(members.size())).getId());
-            if (!seeds.contains(identity)) {
-                seeds.add(identity);
+            var member = members.get(entropy.nextInt(members.size()));
+            var id = View.identityFor(0, endpoints.get(member.getId()), member.getEvent());
+            if (!seeds.contains(id)) {
+                seeds.add(id);
             }
         }
-
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(members.size());
 
         Builder builder = ServerConnectionCache.newBuilder().setTarget(2);
         AtomicBoolean frist = new AtomicBoolean(true);
+        Function<Member, SocketAddress> resolver = m -> {
+            var p = (Participant) m;
+            return p.getIdentity().endpoint();
+        };
+        Function<Member, ClientContextSupplier> clientContextSupplier = m -> {
+            return new ClientContextSupplier() {
+                @Override
+                public SslContext forClient(ClientAuth clientAuth, String alias, CertificateValidator validator,
+                                            Provider provider, String tlsVersion) {
+                    CertificateWithPrivateKey certWithKey = certs.get(m.getId());
+                    return MtlsServer.forClient(clientAuth, alias, certWithKey.getX509Certificate(),
+                                                certWithKey.getPrivateKey(), validator);
+                }
+            };
+        };
         views = members.stream().map(node -> {
             Context<Participant> context = ctxBuilder.build();
             FireflyMetricsImpl metrics = new FireflyMetricsImpl(context.getId(),
                                                                 frist.getAndSet(false) ? node0Registry : registry);
-            Function<Member, SocketAddress> resolver = null;
-            EndpointProvider ep = new StandardEpProvider(null, ClientAuth.REQUIRE, CertificateValidator.NONE, resolver);
+            EndpointProvider ep = new StandardEpProvider(endpoints.get(node.getId()), ClientAuth.REQUIRE,
+                                                         CertificateValidator.NONE, resolver);
             builder.setMetrics(new ServerConnectionCacheMetricsImpl(frist.getAndSet(false) ? node0Registry : registry));
-            MtlsRouter comms = new MtlsRouter(builder, ep, node, Executors.newFixedThreadPool(3));
+            CertificateWithPrivateKey certWithKey = certs.get(node.getId());
+            ServerContextSupplier serverContextSupplier = new ServerContextSupplier() {
+                @Override
+                public Digest getMemberId(X509Certificate key) {
+                    return ((SelfAddressingIdentifier) Stereotomy.decode(key).get().identifier()).getDigest();
+                }
+
+                @Override
+                public SslContext forServer(ClientAuth clientAuth, String alias, CertificateValidator validator,
+                                            Provider provider, String tlsVersion) {
+                    return MtlsServer.forServer(clientAuth, alias, certWithKey.getX509Certificate(),
+                                                certWithKey.getPrivateKey(), validator);
+                }
+            };
+            MtlsRouter comms = new MtlsRouter(builder, ep, serverContextSupplier, Executors.newFixedThreadPool(3),
+                                              clientContextSupplier);
             communications.add(comms);
-            return new View(context, node, EventValidation.NONE, comms, 0.0125, DigestAlgorithm.DEFAULT, metrics);
+            return new View(context, node, endpoints.get(node.getId()), EventValidation.NONE, comms, 0.0125,
+                            DigestAlgorithm.DEFAULT, metrics);
         }).collect(Collectors.toList());
 
         long then = System.currentTimeMillis();
