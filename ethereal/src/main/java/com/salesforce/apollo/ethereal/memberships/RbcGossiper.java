9e69780a81a36332529d7d793db51fe9c5c0861e
Hellblazer <hellblazer@me.com>
2022-04-08 12:47:01 -0700

    first sketch of RBC gossip

---
+++ ethereal/src/main/java/com/salesforce/apollo/ethereal/memberships/RbcGossiper.java
@@ -0,0 +1,298 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+package com.salesforce.apollo.ethereal.memberships;
+
+import static com.salesforce.apollo.ethereal.memberships.GossiperClient.getCreate;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.codahale.metrics.Timer;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.salesfoce.apollo.ethereal.proto.ContextUpdate;
+import com.salesfoce.apollo.ethereal.proto.Gossip;
+import com.salesfoce.apollo.ethereal.proto.Have;
+import com.salesfoce.apollo.ethereal.proto.PreUnit_s;
+import com.salesfoce.apollo.ethereal.proto.SignedCommit;
+import com.salesfoce.apollo.ethereal.proto.SignedPreVote;
+import com.salesfoce.apollo.ethereal.proto.Update;
+import com.salesfoce.apollo.ethereal.proto.Update.Builder;
+import com.salesfoce.apollo.utils.proto.Biff;
+import com.salesforce.apollo.comm.RingCommunications;
+import com.salesforce.apollo.comm.Router;
+import com.salesforce.apollo.comm.Router.CommonCommunications;
+import com.salesforce.apollo.crypto.Digest;
+import com.salesforce.apollo.ethereal.Config;
+import com.salesforce.apollo.membership.Context;
+import com.salesforce.apollo.membership.Member;
+import com.salesforce.apollo.membership.SigningMember;
+import com.salesforce.apollo.utils.Utils;
+import com.salesforce.apollo.utils.bloomFilters.BloomFilter;
+import com.salesforce.apollo.utils.bloomFilters.BloomFilter.DigestBloomFilter;
+
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+
+/**
+ * @author hal.hildebrand
+ *
+ */
+public class RbcGossiper {
+    private class Terminal implements GossiperService {
+        @Override
+        public Update gossip(Gossip request, Digest from) {
+            Member predecessor = context.ring(request.getRing()).predecessor(member);
+            if (predecessor == null || !from.equals(predecessor.getId())) {
+                log.debug("Invalid inbound gossip on {}:{} from: {} on ring: {} - not predecessor: {}", context.getId(),
+                          member, from, request.getRing(), predecessor);
+                return Update.getDefaultInstance();
+            }
+            final var update = RbcGossiper.this.gossip(request.getHaves());
+            log.trace("GossipService received from: {} missing: {} on: {}", from, update.getMissingCount(), member);
+            return update;
+        }
+
+        @Override
+        public void update(ContextUpdate request, Digest from) {
+            Member predecessor = context.ring(request.getRing()).predecessor(member);
+            if (predecessor == null || !from.equals(predecessor.getId())) {
+                log.debug("Invalid inbound update on {}:{} from: {} on ring: {} - not predecessor: {}", context.getId(),
+                          member, from, request.getRing(), predecessor);
+                return;
+            }
+            log.debug("gossip update with {} on: {}", from, member);
+            RbcGossiper.this.update(request.getUpdate());
+        }
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(RbcGossiper.class);
+
+    private final CommonCommunications<Gossiper, GossiperService> comm;
+    private final Map<Digest, SignedCommit>                       commits  = new ConcurrentHashMap<>();
+    private final Config                                          config;
+    private final Context<Member>                                 context;
+    private final SigningMember                                   member;
+    private final EtherealMetrics                                 metrics;
+    private final Map<Digest, SignedPreVote>                      preVotes = new ConcurrentHashMap<>();
+    private final RingCommunications<Gossiper>                    ring;
+    private final AtomicInteger                                   round    = new AtomicInteger();
+    private volatile ScheduledFuture<?>                           scheduled;
+    private final AtomicBoolean                                   started  = new AtomicBoolean();
+    private final Map<Digest, PreUnit_s>                          units    = new ConcurrentHashMap<>();
+
+    public RbcGossiper(Context<Member> context, SigningMember member, Config config, Router communications,
+                       Executor exec, EtherealMetrics m) {
+        this.context = context;
+        this.member = member;
+        this.metrics = m;
+        this.config = config;
+        comm = communications.create((Member) member, context.getId(), new Terminal(),
+                                     r -> new GossiperServer(communications.getClientIdentityProvider(), metrics, r),
+                                     getCreate(metrics), Gossiper.getLocalLoopback(member));
+        final var cast = context;
+        ring = new RingCommunications<Gossiper>(cast, member, this.comm, exec);
+    }
+
+    public Context<Member> getContext() {
+        return context;
+    }
+
+    public void nextRound() {
+        round.incrementAndGet();
+    }
+
+    public void start(Duration duration, ScheduledExecutorService scheduler) {
+        if (!started.compareAndSet(false, true)) {
+            return;
+        }
+        Duration initialDelay = duration.plusMillis(Utils.bitStreamEntropy().nextLong(2 * duration.toMillis()));
+        log.trace("Starting GossipService[{}] on: {}", context.getId(), member);
+        comm.register(context.getId(), new Terminal());
+        scheduler.schedule(() -> {
+            try {
+                oneRound(duration, scheduler);
+            } catch (Throwable e) {
+                log.error("Error in gossip on: {}", member, e);
+            }
+        }, initialDelay.toMillis(), TimeUnit.MILLISECONDS);
+    }
+
+    public void stop() {
+        if (!started.compareAndSet(true, false)) {
+            return;
+        }
+        log.trace("Stopping GossipService [{}] for {}", context.getId(), member);
+        comm.deregister(context.getId());
+        final var current = scheduled;
+        scheduled = null;
+        if (current != null) {
+            current.cancel(true);
+        }
+    }
+
+    private Update gossip(Have have) {
+        final var builder = Update.newBuilder().setHaves(have());
+        update(have, builder);
+        return builder.build();
+    }
+
+    private ListenableFuture<Update> gossipRound(Gossiper link, int ring) {
+        if (!started.get()) {
+            return null;
+        }
+        log.debug("gossiping[{}] with {} ring: {} on {}", context.getId(), link.getMember(), ring, member);
+        try {
+            return link.gossip(Gossip.newBuilder().setContext(context.getId().toDigeste()).setHaves(have()).build());
+        } catch (StatusRuntimeException e) {
+            log.debug("gossiping[{}] failed with: {} with {} ring: {} on {}", context.getId(), e.getMessage(), member,
+                      ring, link.getMember(), e);
+            return null;
+        } catch (Throwable e) {
+            log.warn("gossiping[{}] failed from {} with {} ring: {} on {}", context.getId(), member, ring,
+                     link.getMember(), e);
+            return null;
+        }
+    }
+
+    private void handle(Optional<ListenableFuture<Update>> futureSailor, Gossiper link, int ring, Duration duration,
+                        ScheduledExecutorService scheduler, Timer.Context timer) {
+        if (!started.get() || link == null) {
+            if (timer != null) {
+                timer.stop();
+            }
+            return;
+        }
+        try {
+            if (futureSailor.isEmpty()) {
+                if (timer != null) {
+                    timer.stop();
+                }
+                log.trace("no update from {} on: {}", link.getMember(), member);
+                return;
+            }
+            Update update;
+            try {
+                update = futureSailor.get().get();
+            } catch (InterruptedException e) {
+                log.error("error gossiping with {} on: {}", link.getMember(), member, e);
+                return;
+            } catch (ExecutionException e) {
+                var cause = e.getCause();
+                if (cause instanceof StatusRuntimeException sre) {
+                    final var code = sre.getStatus().getCode();
+                    if (code.equals(Status.UNAVAILABLE.getCode()) || code.equals(Status.NOT_FOUND.getCode()) ||
+                        code.equals(Status.UNIMPLEMENTED.getCode())) {
+                        return;
+                    }
+                }
+                log.warn("error gossiping with {} on: {}", link.getMember(), member, cause);
+                return;
+            }
+            log.debug("gossip update with {} on: {}", link.getMember(), member);
+            link.update(ContextUpdate.newBuilder()
+                                     .setContext(context.getId().toDigeste())
+                                     .setRing(ring)
+                                     .setUpdate(update(update))
+                                     .build());
+        } finally {
+            if (timer != null) {
+                timer.stop();
+            }
+            if (started.get()) {
+                scheduled = scheduler.schedule(() -> oneRound(duration, scheduler), duration.toMillis(),
+                                               TimeUnit.MILLISECONDS);
+            }
+        }
+    }
+
+    private Have have() {
+        return Have.newBuilder()
+                   .setHaveCommits(haveCommits())
+                   .setHavePreVotes(havePreVotes())
+                   .setHaveUnits(haveUnits())
+                   .build();
+    }
+
+    private Biff haveCommits() {
+        var biff = new DigestBloomFilter(Utils.bitStreamEntropy().nextLong(), config.epochLength()
+        * config.numberOfEpochs() * config.nProc() * config.nProc() * 2, config.fpr());
+        commits.keySet().forEach(h -> biff.add(h));
+        return biff.toBff();
+    }
+
+    private Biff havePreVotes() {
+        var biff = new DigestBloomFilter(Utils.bitStreamEntropy().nextLong(), config.epochLength()
+        * config.numberOfEpochs() * config.nProc() * config.nProc() * 2, config.fpr());
+        preVotes.keySet().forEach(h -> biff.add(h));
+        return biff.toBff();
+    }
+
+    private Biff haveUnits() {
+        var biff = new DigestBloomFilter(Utils.bitStreamEntropy().nextLong(),
+                                         config.epochLength() * config.numberOfEpochs() * config.nProc() * 2,
+                                         config.fpr());
+        units.keySet().forEach(h -> biff.add(h));
+        return biff.toBff();
+    }
+
+    private void oneRound(Duration duration, ScheduledExecutorService scheduler) {
+        if (!started.get()) {
+            return;
+        }
+        var timer = metrics == null ? null : metrics.gossipRoundDuration().time();
+        ring.execute((link, ring) -> gossipRound(link, ring),
+                     (futureSailor, link, ring) -> handle(futureSailor, link, ring, duration, scheduler, timer));
+    }
+
+    private void update(Have have, final Builder builder) {
+        updateCommits(BloomFilter.from(have.getHaveCommits()), builder);
+        updatePreVotes(BloomFilter.from(have.getHavePreVotes()), builder);
+        updateUnits(BloomFilter.from(have.getHaveUnits()), builder);
+    }
+
+    private Update update(Update update) {
+        final var builder = Update.newBuilder().setHaves(have());
+        update(update.getHaves(), builder);
+        return builder.build();
+    }
+
+    private void updateCommits(BloomFilter<Digest> have, Update.Builder builder) {
+        commits.entrySet().forEach(e -> {
+            if (!have.contains(e.getKey())) {
+                builder.addMissingCommits(e.getValue());
+            }
+        });
+    }
+
+    private void updatePreVotes(BloomFilter<Object> have, Builder builder) {
+        preVotes.entrySet().forEach(e -> {
+            if (!have.contains(e.getKey())) {
+                builder.addMissingPreVotes(e.getValue());
+            }
+        });
+    }
+
+    private void updateUnits(BloomFilter<Object> have, Builder builder) {
+        units.entrySet().forEach(e -> {
+            if (!have.contains(e.getKey())) {
+                builder.addMissing(e.getValue());
+            }
+        });
+    }
+}
---
+++ ethereal/src/test/java/com/salesforce/apollo/ethereal/memberships/RbcGossiperTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2022, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+package com.salesforce.apollo.ethereal.memberships;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+
+import com.salesforce.apollo.comm.LocalRouter;
+import com.salesforce.apollo.comm.Router;
+import com.salesforce.apollo.comm.ServerConnectionCache;
+import com.salesforce.apollo.crypto.Verifier;
+import com.salesforce.apollo.ethereal.Config;
+import com.salesforce.apollo.membership.Context;
+import com.salesforce.apollo.membership.SigningMember;
+import com.salesforce.apollo.membership.impl.SigningMemberImpl;
+import com.salesforce.apollo.utils.Utils;
+
+/**
+ * @author hal.hildebrand
+ *
+ */
+public class RbcGossiperTest {
+
+    @Test
+    public void smokin() {
+        String prefix = UUID.randomUUID().toString();
+        Executor exec = Executors.newFixedThreadPool(2);
+        var context = Context.newBuilder().setCardinality(10).build();
+        List<SigningMember> members = IntStream.range(0, 4)
+                                               .mapToObj(i -> (SigningMember) new SigningMemberImpl(Utils.getMember(0)))
+                                               .toList();
+        members.forEach(m -> context.activate(m));
+        var builder = Config.deterministic()
+                            .setnProc((short) members.size())
+                            .setVerifiers(members.toArray(new Verifier[members.size()]));
+        List<RbcGossiper> gossipers = new ArrayList<>();
+        for (short i = 0; i < members.size(); i++) {
+            Router comm = new LocalRouter(prefix, ServerConnectionCache.newBuilder(), exec, null);
+            gossipers.add(new RbcGossiper(context, members.get(i), builder.setSigner(members.get(i)).setPid(i).build(),
+                                          comm, exec, null));
+        }
+    }
+}
