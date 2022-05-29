c4d7340ebe08aff3383864434d03e38d54baa27c
Hellblazer <hellblazer@me.com>
2022-05-28 22:41:54 -0700

    Saksi and Validator

--- thoth/src/main/java/com/salesforce/apollo/thoth/Sakshi.java
+++ thoth/src/main/java/com/salesforce/apollo/thoth/Sakshi.java
@@ -6,18 +6,22 @@
  */
 package com.salesforce.apollo.thoth;
 
-import java.security.KeyPair;
+import java.util.List;
 import java.util.Optional;
 
-import org.h2.jdbcx.JdbcConnectionPool;
-
+import com.salesfoce.apollo.stereotomy.event.proto.Ident;
+import com.salesfoce.apollo.stereotomy.event.proto.KeyEventWithAttachments;
+import com.salesfoce.apollo.stereotomy.event.proto.KeyEvent_;
+import com.salesfoce.apollo.thoth.proto.Signatures;
+import com.salesfoce.apollo.thoth.proto.Validated;
+import com.salesfoce.apollo.utils.proto.Sig;
 import com.salesforce.apollo.crypto.JohnHancock;
 import com.salesforce.apollo.crypto.Signer;
 import com.salesforce.apollo.stereotomy.ControlledIdentifier;
-import com.salesforce.apollo.stereotomy.KERL;
-import com.salesforce.apollo.stereotomy.Stereotomy;
-import com.salesforce.apollo.stereotomy.event.KeyEvent;
+import com.salesforce.apollo.stereotomy.event.protobuf.InteractionEventImpl;
+import com.salesforce.apollo.stereotomy.event.protobuf.ProtobufEventFactory;
 import com.salesforce.apollo.stereotomy.identifier.BasicIdentifier;
+import com.salesforce.apollo.stereotomy.identifier.Identifier;
 
 /**
  * The witness and validation service in Thoth
@@ -26,36 +30,67 @@ import com.salesforce.apollo.stereotomy.identifier.BasicIdentifier;
  *
  */
 public class Sakshi {
+    private final ControlledIdentifier<? super Identifier> validator;
+    private final ControlledIdentifier<BasicIdentifier>    witness;
 
-    @SuppressWarnings("unused")
-    private final JdbcConnectionPool         jdbcPool;
-    @SuppressWarnings("unused")
-    private final Stereotomy                 stereotomy;
-    private volatile ControlledIdentifier<?> validator;
-    private volatile KeyPair                 witness;
-
-    public Sakshi(KERL kerl, Stereotomy stereotomy, JdbcConnectionPool jdbcPool) {
-        this.stereotomy = stereotomy;
-        this.jdbcPool = jdbcPool;
+    public Sakshi(ControlledIdentifier<? super Identifier> validator, ControlledIdentifier<BasicIdentifier> witness) {
+        this.validator = validator;
+        this.witness = witness;
     }
 
-    public BasicIdentifier getWitness() {
-        KeyPair current = witness;
-        return new BasicIdentifier(current.getPublic());
+    public Ident getWitness() {
+        return witness.getIdentifier().toIdent();
     }
 
-    public JohnHancock validate(KeyEvent event) {
+    public Validated validate(List<KeyEventWithAttachments> events) {
+        final var builder = Validated.newBuilder();
+        events.forEach(event -> {
+            var valid = validate(event);
+            if (valid.isEmpty()) {
+                builder.addSignatures(Sig.getDefaultInstance());
+            } else {
+                builder.addSignatures(valid.get().toSig()).build();
+            }
+        });
+        return builder.build();
+    }
+
+    public Optional<Sig> witness(KeyEvent_ evente, Ident identifier) {
+        if (!witness.getIdentifier().toIdent().equals(identifier)) {
+            return Optional.empty();
+        }
+        Optional<Signer> signer = witness.getSigner();
+        if (signer.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(signer.get().sign(evente.toByteString()).toSig());
+    }
+
+    private Optional<JohnHancock> validate(KeyEventWithAttachments kea) {
+        if (!witnessed(kea)) {
+            return Optional.empty();
+        }
         Optional<Signer> signer = validator.getSigner();
         if (signer.isEmpty()) {
-            return null;
+            return Optional.empty();
         }
-        return signer.get().sign(event.toKeyEvent_().toByteString());
+        KeyEvent_ evente = switch (kea.getEventCase()) {
+        case INCEPTION -> ProtobufEventFactory.toKeyEvent(kea.getInception()).toKeyEvent_();
+        case INTERACTION -> new InteractionEventImpl(kea.getInteraction()).toKeyEvent_();
+        case ROTATION -> ProtobufEventFactory.toKeyEvent(kea.getRotation()).toKeyEvent_();
+        default -> null;
+        };
+        return (evente == null) ? Optional.empty() : Optional.of(signer.get().sign(evente.toByteString()));
     }
 
-    public JohnHancock witness(KeyEvent event, BasicIdentifier identifier) {
-        if (!witness.getPublic().equals(identifier.getPublicKey())) {
-            return null;
-        }
-        return new Signer.SignerImpl(witness.getPrivate()).sign(event.toKeyEvent_().toByteString());
+    // Confirm that the attachments witness the event
+    private boolean witnessed(KeyEventWithAttachments kea) {
+        // TODO
+        return true;
+    }
+
+    public Signatures witness(List<KeyEvent_> keyEventList, Ident identifier) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
