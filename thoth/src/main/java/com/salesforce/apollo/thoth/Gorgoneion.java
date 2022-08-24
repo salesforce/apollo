b6a115f553ecc93f5b5bcd9c95b17f5573bf252f
Hellblazer <hellblazer@me.com>
2022-08-23 22:48:06 -0700

    moar

--- thoth/src/main/java/com/salesforce/apollo/thoth/Gorgoneion.java
+++ thoth/src/main/java/com/salesforce/apollo/thoth/Gorgoneion.java
@@ -8,18 +8,23 @@ package com.salesforce.apollo.thoth;
 
 import java.security.SecureRandom;
 import java.time.Clock;
+import java.time.Duration;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Predicate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.protobuf.Duration;
 import com.salesfoce.apollo.thoth.proto.AdmissionsGossip;
 import com.salesfoce.apollo.thoth.proto.AdmissionsUpdate;
 import com.salesfoce.apollo.thoth.proto.Admittance;
@@ -27,6 +32,7 @@ import com.salesfoce.apollo.thoth.proto.Expunge;
 import com.salesfoce.apollo.thoth.proto.Registration;
 import com.salesfoce.apollo.thoth.proto.SignedAttestation;
 import com.salesfoce.apollo.thoth.proto.SignedNonce;
+import com.salesfoce.apollo.utils.proto.Biff;
 import com.salesforce.apollo.comm.RingCommunications;
 import com.salesforce.apollo.comm.RingCommunications.Destination;
 import com.salesforce.apollo.comm.Router;
@@ -47,6 +53,7 @@ import com.salesforce.apollo.thoth.grpc.admission.gossip.AdmissionsReplication;
 import com.salesforce.apollo.thoth.grpc.admission.gossip.AdmissionsReplicationClient;
 import com.salesforce.apollo.thoth.grpc.admission.gossip.AdmissionsReplicationServer;
 import com.salesforce.apollo.utils.Entropy;
+import com.salesforce.apollo.utils.RoundScheduler;
 import com.salesforce.apollo.utils.Utils;
 import com.salesforce.apollo.utils.bloomFilters.BloomFilter;
 import com.salesforce.apollo.utils.bloomFilters.BloomFilter.DigestBloomFilter;
@@ -102,20 +109,22 @@ public class Gorgoneion {
     private final DigestAlgorithm                                                          digestAlgo;
     private final SecureRandom                                                             entropy;
     private final Executor                                                                 exec;
-    private final ConcurrentNavigableMap<Digest, Expunge>                                  expunged    = new ConcurrentSkipListMap<>();
+    private final ConcurrentSkipListSet<Digest>                                            expunged    = new ConcurrentSkipListSet<>();
     private final double                                                                   fpr;
+    private volatile ScheduledFuture<?>                                                    futureGossip;
     private final RingCommunications<Member, AdmissionReplicationService>                  gossiper;
     private final KERL                                                                     kerl;
     private final ControlledIdentifierMember                                               member;
     private final ConcurrentNavigableMap<Digest, SignedNonce>                              pending     = new ConcurrentSkipListMap<>();
     private final AdmissionsReplication                                                    replication = new Replication();
     private final CommonCommunications<AdmissionReplicationService, AdmissionsReplication> replicationComms;
-
-    private final AtomicBoolean started = new AtomicBoolean();
+    private final RoundScheduler                                                           roundTimers;
+    private final AtomicBoolean                                                            started     = new AtomicBoolean();
+    private final Predicate<SignedAttestation>                                             validator;
 
     public Gorgoneion(ControlledIdentifierMember member, Context<Member> context, Router admissionsRouter, KERL kerl,
                       Router replicationRouter, Executor executor, Clock clock, SecureRandom entropy,
-                      DigestAlgorithm digestAlgo, double fpr) {
+                      DigestAlgorithm digestAlgo, double fpr, Predicate<SignedAttestation> validator) {
         this.clock = clock;
         this.digestAlgo = digestAlgo;
         this.entropy = entropy;
@@ -137,6 +146,8 @@ public class Gorgoneion {
                                                   AdmissionClient.getCreate(context.getId(), null),
                                                   AdmissionClient.getLocalLoopback(admissions, member));
         gossiper = new RingCommunications<>(context, member, replicationComms, executor);
+        roundTimers = new RoundScheduler("replications", context.timeToLive());
+        this.validator = validator;
     }
 
     public void start(Duration frequency, ScheduledExecutorService scheduler) {
@@ -146,15 +157,37 @@ public class Gorgoneion {
         gossip(frequency, scheduler);
     }
 
+    public void stop() {
+        if (!started.compareAndExchange(true, false)) {
+            return;
+        }
+        final var current = futureGossip;
+        futureGossip = null;
+        if (current != null) {
+            futureGossip.cancel(true);
+        }
+    }
+
+    private void add(SignedNonce sn) {
+        final var digest = JohnHancock.from(sn.getSignature()).toDigest(digestAlgo);
+        if (expunged.contains(digest)) {
+            log.trace("Not adding expunged: {} on: {}", digest, member.getId());
+            return;
+        }
+        pending.putIfAbsent(digest, sn);
+    }
+
     private BloomFilter<Digest> getBff(long seed, double p) {
         var biff = new DigestBloomFilter(seed, Math.min(100, pending.size()), p);
-        pending.entrySet()
-               .forEach(e -> biff.add(e.getKey()
-                                       .prefix(JohnHancock.from(e.getValue().getSignature()).toDigest(digestAlgo))));
+        pending.keySet().forEach(e -> biff.add(e));
         return biff;
     }
 
     private ListenableFuture<AdmissionsUpdate> gossip(AdmissionReplicationService link, Integer ring) {
+        if (!started.get()) {
+            return null;
+        }
+        roundTimers.tick();
         return link.gossip(AdmissionsGossip.newBuilder()
                                            .setBff(getBff(Entropy.nextBitsStreamLong(), fpr).toBff())
                                            .build());
@@ -164,16 +197,54 @@ public class Gorgoneion {
         if (!started.get()) {
             return;
         }
-        exec.execute(Utils.wrapped(() -> gossiper.execute((link, ring) -> gossip(link, ring),
-                                                          (futureSailor, destination) -> gossip(futureSailor,
-                                                                                                destination, frequency,
-                                                                                                scheduler)),
-                                   log));
+        exec.execute(Utils.wrapped(() -> {
+            if (context.activeCount() == 1) {
+                roundTimers.tick();
+            }
+            gossiper.execute((link, ring) -> gossip(link, ring),
+                             (futureSailor, destination) -> gossip(futureSailor, destination, frequency, scheduler));
+        }, log));
     }
 
     private void gossip(Optional<ListenableFuture<AdmissionsUpdate>> futureSailor,
                         Destination<Member, AdmissionReplicationService> destination, Duration frequency,
                         ScheduledExecutorService scheduler) {
+        final var member = destination.member();
+        try {
+            if (futureSailor.isEmpty()) {
+                return;
+            }
+            AdmissionsUpdate update;
+            try {
+                update = futureSailor.get().get();
+                update.getUpdateList().forEach(sn -> add(sn));
+            } catch (ExecutionException e) {
+                log.trace("Error in gossip on: {}", member.getId());
+                return;
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+            final var bff = update.getBff();
+            if (Biff.getDefaultInstance().equals(bff)) {
+                return;
+            }
+            var response = updateFor(BloomFilter.from(bff));
+            if (response == null) {
+                return;
+            }
+        } finally {
+            futureGossip = scheduler.schedule(() -> gossip(frequency, scheduler), frequency.toNanos(),
+                                              TimeUnit.NANOSECONDS);
+        }
+    }
 
+    private AdmissionsUpdate updateFor(BloomFilter<Digest> have) {
+        var update = AdmissionsUpdate.newBuilder();
+        pending.entrySet()
+               .stream()
+               .filter(e -> !have.contains(e.getKey()))
+               .forEach(e -> update.addUpdate(e.getValue()));
+        return update.build();
     }
 }
