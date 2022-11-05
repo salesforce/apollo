14774bfa3180d50c64a53711103659e468af48f3
Hellblazer <hellblazer@me.com>
2022-11-04 18:34:53 -0700

    validation api, added constraint checking on functions.

--- utils/src/main/java/com/salesforce/apollo/crypto/HexBloom.java
+++ utils/src/main/java/com/salesforce/apollo/crypto/HexBloom.java
@@ -8,9 +8,11 @@ package com.salesforce.apollo.crypto;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import com.salesfoce.apollo.utils.proto.HexBloome;
 import com.salesforce.apollo.utils.Entropy;
@@ -88,7 +90,10 @@ public class HexBloom {
      */
     public static HexBloom construct(List<Digest> currentMembership, List<Digest> added, List<Digest> crowns,
                                      List<Digest> removed, List<Function<Digest, Digest>> hashes, double fpr) {
-        assert crowns.size() == hashes.size();
+        if (hashes.size() != crowns.size()) {
+            throw new IllegalArgumentException("Size of supplied hash functions: " + hashes.size()
+            + " must equal the # of crowns: " + crowns.size());
+        }
         var cardinality = currentMembership.size() + added.size() - removed.size();
         var membership = new BloomFilter.DigestBloomFilter(Entropy.nextSecureLong(), cardinality, fpr);
         var crwns = crowns.stream().map(d -> new AtomicReference<>(d)).toList();
@@ -207,6 +212,10 @@ public class HexBloom {
      * @return
      */
     public HexBloome toHexBloome(List<Function<Digest, Digest>> hashes) {
+        if (hashes.size() != crowns.length) {
+            throw new IllegalArgumentException("Size of supplied hash functions: " + hashes.size()
+            + " must equal the # of crowns: " + crowns.length);
+        }
         final var builder = HexBloome.newBuilder().setCardinality(cardinality).setMembership(membership.toBff());
         for (int i = 0; i < crowns.length; i++) {
             builder.addCrowns(hashes.get(i).apply(crowns[i]).toDigeste());
@@ -221,5 +230,78 @@ public class HexBloom {
      */
     public HexBloome toIdentityHexBloome() {
         return toHexBloome(IntStream.range(0, crowns.length).mapToObj(i -> IDENTITY).toList());
+    }
+
+    /**
+     * Validate that the supplied members matches the receiver's crowns. All members
+     * must be included in the membership bloomfilter, all calculated crowns must
+     * match and the cardinality must match.
+     *
+     * @param members - list of member digests
+     * @return true if validated
+     */
+    public boolean validate(List<Digest> members) {
+        return validate(members.stream(), hashes(crowns.length));
+    }
+
+    /**
+     * Validate that the supplied members matches the receiver's crowns. All members
+     * must be included in the membership bloomfilter, all calculated crowns must
+     * match and the cardinality must match.
+     *
+     * @param members - lsit of member digests
+     * @param hashes  - hash functions for computing crowns
+     * @return true if validated
+     */
+    public boolean validate(List<Digest> members, List<Function<Digest, Digest>> hashes) {
+        return validate(members.stream(), hashes);
+    }
+
+    /**
+     * Validate that the supplied members matches the receiver's crowns. All members
+     * must be included in the membership bloomfilter, all calculated crowns must
+     * match and the cardinality must match.
+     *
+     * @param members - stream of member digests
+     * @return true if validated
+     */
+    public boolean validate(Stream<Digest> members) {
+        return validate(members, hashes(crowns.length));
+    }
+
+    /**
+     * Validate that the supplied members matches the receiver's crowns. All members
+     * must be included in the membership bloomfilter, all calculated crowns must
+     * match and the cardinality must match.
+     *
+     * @param members - stream of member digests
+     * @param hashes  - hash functions for computing crowns
+     * @return true if validated
+     */
+    public boolean validate(Stream<Digest> members, List<Function<Digest, Digest>> hashes) {
+        if (hashes.size() != crowns.length) {
+            throw new IllegalArgumentException("Size of supplied hash functions: " + hashes.size()
+            + " must equal the # of crowns: " + crowns.length);
+        }
+        var count = new AtomicInteger();
+        @SuppressWarnings("unchecked")
+        AtomicReference<Digest>[] calculated = (AtomicReference<Digest>[]) IntStream.range(0, crowns.length)
+                                                                                    .mapToObj(i -> new AtomicReference<Digest>())
+                                                                                    .toArray();
+        members.forEach(d -> {
+            for (int i = 0; i < crowns.length; i++) {
+                calculated[i].accumulateAndGet(hashes.get(i).apply(d), (a, b) -> a.xor(b));
+            }
+            count.incrementAndGet();
+        });
+        if (count.get() != cardinality) {
+            return false;
+        }
+        for (int i = 0; i < crowns.length; i++) {
+            if (!calculated[i].get().equals(crowns[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
