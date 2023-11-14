3449cca80e0c61d8254e2851124b27a803b5df8c
Constantine <hellblazer@me.com>
2023-11-14 10:47:51 -0800

    labels (#159)

--- protocols/src/main/java/com/salesforce/apollo/protocols/EndpointMetrics.java
+++ protocols/src/main/java/com/salesforce/apollo/protocols/EndpointMetrics.java
@@ -14,8 +14,8 @@ import com.codahale.metrics.Meter;
  */
 public interface EndpointMetrics {
 
-    String INBOUND_BANDWIDTH  = "bandwidth.inbound";
-    String OUTBOUND_BANDWIDTH = "bandwidth.outbound";
+    String INBOUND_BANDWIDTH  = "bandwidth.inbound.bytes";
+    String OUTBOUND_BANDWIDTH = "bandwidth.outbound.bytes";
 
     Meter inboundBandwidth();
 
