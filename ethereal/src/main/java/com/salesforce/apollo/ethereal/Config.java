b5a9bf514eb2ff9dec96570fe51fb90a8527ca10
Hellblazer <hellblazer@me.com>
2024-02-06 17:08:40 -0800

    remove unused

--- ethereal/src/main/java/com/salesforce/apollo/ethereal/Config.java
+++ ethereal/src/main/java/com/salesforce/apollo/ethereal/Config.java
@@ -23,11 +23,15 @@ import java.util.Objects;
  * @author hal.hildebrand
  */
 public record Config(String label, short nProc, int epochLength, short pid, Signer signer,
-                     DigestAlgorithm digestAlgorithm, int lastLevel, int numberOfEpochs, WeakThresholdKey WTKey,
-                     double bias, Verifier[] verifiers, double fpr) {
+                     DigestAlgorithm digestAlgorithm, int numberOfEpochs, WeakThresholdKey WTKey, double bias,
+                     Verifier[] verifiers, double fpr) {
 
     public static Builder newBuilder() {
         return new Builder();
+    }
+
+    public int lastLevel() {
+        return epochLength - 1;
     }
 
     public String logLabel() {
@@ -62,8 +66,8 @@ public record Config(String label, short nProc, int epochLength, short pid, Sign
             }
             Objects.requireNonNull(signer, "Signer cannot be null");
             Objects.requireNonNull(digestAlgorithm, "Digest Algorithm cannot be null");
-            return new Config(label, nProc, epochLength, pid, signer, digestAlgorithm, epochLength - 1, numberOfEpochs,
-                              wtk, bias, verifiers, fpr);
+            return new Config(label, nProc, epochLength, pid, signer, digestAlgorithm, numberOfEpochs, wtk, bias,
+                              verifiers, fpr);
         }
 
         @Override
@@ -120,30 +124,12 @@ public record Config(String label, short nProc, int epochLength, short pid, Sign
             return this;
         }
 
-        public short getnProc() {
-            return nProc;
-        }
-
-        public Builder setnProc(short nProc) {
-            this.nProc = nProc;
-            return this;
-        }
-
         public int getNumberOfEpochs() {
             return numberOfEpochs;
         }
 
         public Builder setNumberOfEpochs(int numberOfEpochs) {
             this.numberOfEpochs = numberOfEpochs;
-            return this;
-        }
-
-        public double getpByz() {
-            return pByz;
-        }
-
-        public Builder setpByz(double pByz) {
-            this.pByz = pByz;
             return this;
         }
 
@@ -180,6 +166,24 @@ public record Config(String label, short nProc, int epochLength, short pid, Sign
 
         public Builder setWtk(WeakThresholdKey wtk) {
             this.wtk = wtk;
+            return this;
+        }
+
+        public short getnProc() {
+            return nProc;
+        }
+
+        public Builder setnProc(short nProc) {
+            this.nProc = nProc;
+            return this;
+        }
+
+        public double getpByz() {
+            return pByz;
+        }
+
+        public Builder setpByz(double pByz) {
+            this.pByz = pByz;
             return this;
         }
     }
