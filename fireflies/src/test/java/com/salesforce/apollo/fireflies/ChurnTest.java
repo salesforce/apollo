07832b63133d1956037a1723e7b89f281879fbf0
Hellblazer <hellblazer@me.com>
2024-06-06 14:53:04 -0700

    ahem no

--- fireflies/src/test/java/com/salesforce/apollo/fireflies/ChurnTest.java
+++ fireflies/src/test/java/com/salesforce/apollo/fireflies/ChurnTest.java
@@ -273,7 +273,6 @@ public class ChurnTest {
         AtomicBoolean frist = new AtomicBoolean(true);
         final var prefix = UUID.randomUUID().toString();
         final var gatewayPrefix = UUID.randomUUID().toString();
-        final var executor = UnsafeExecutors.newVirtualThreadPerTaskExecutor();
         views = members.values().stream().map(node -> {
             DynamicContext<Participant> context = ctxBuilder.build();
             FireflyMetricsImpl metrics = new FireflyMetricsImpl(context.getId(),
@@ -283,8 +282,7 @@ public class ChurnTest {
                                                                                   .setMetrics(
                                                                                   new ServerConnectionCacheMetricsImpl(
                                                                                   frist.getAndSet(false) ? node0Registry
-                                                                                                         : registry)),
-                                                             executor);
+                                                                                                         : registry)));
             var gateway = new LocalServer(gatewayPrefix, node).router(ServerConnectionCache.newBuilder()
                                                                                            .setTarget(200)
                                                                                            .setMetrics(
