a453f4fb0b5224e8736f948a564084553bfdfbf4
Hellblazer <hellblazer@me.com>
2024-06-08 07:43:42 -0700

    revert

--- fireflies/src/test/java/com/salesforce/apollo/fireflies/SwarmTest.java
+++ fireflies/src/test/java/com/salesforce/apollo/fireflies/SwarmTest.java
@@ -29,6 +29,7 @@ import java.security.SecureRandom;
 import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
@@ -62,6 +63,7 @@ public class SwarmTest {
     private       MetricRegistry                          node0Registry;
     private       MetricRegistry                          registry;
     private       List<View>                              views;
+    private       ExecutorService                         executor;
 
     @BeforeAll
     public static void beforeClass() throws Exception {
@@ -89,6 +91,9 @@ public class SwarmTest {
 
         gateways.forEach(e -> e.close(Duration.ofSeconds(1)));
         gateways.clear();
+        if (executor != null) {
+            executor.shutdown();
+        }
     }
 
     @Test
@@ -204,6 +209,7 @@ public class SwarmTest {
     }
 
     private void initialize() {
+        executor = UnsafeExecutors.newVirtualThreadPerTaskExecutor();
         var parameters = Parameters.newBuilder()
                                    .setMaxPending(50)
                                    .setMaximumTxfr(20)
@@ -235,14 +241,15 @@ public class SwarmTest {
                                                                                   .setMetrics(
                                                                                   new ServerConnectionCacheMetricsImpl(
                                                                                   frist.getAndSet(false) ? node0Registry
-                                                                                                         : registry)));
+                                                                                                         : registry)),
+                                                             executor);
             var gateway = new LocalServer(gatewayPrefix, node).router(ServerConnectionCache.newBuilder()
                                                                                            .setTarget(200)
                                                                                            .setMetrics(
                                                                                            new ServerConnectionCacheMetricsImpl(
                                                                                            frist.getAndSet(false)
-                                                                                           ? node0Registry
-                                                                                           : registry)));
+                                                                                           ? node0Registry : registry)),
+                                                                      executor);
             comms.start();
             communications.add(comms);
 
